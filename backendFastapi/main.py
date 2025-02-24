9c6998f05ca74d7653e8811c41bdd3e862f70297
Sheron <sheronroy2003@gmail.com>
2025-02-24 21:23:28 +0530

    Geoip service added

---
+++ backendFastapi/main.py
@@ -0,0 +1,16 @@
+# main.py
+from fastapi import FastAPI
+from services.geoip_service import geoip_router
+"""from services.user_input_service import user_input_router
+from services.ai_model_service import ai_model_router"""
+
+app = FastAPI()
+
+# Registering the routers
+app.include_router(geoip_router, prefix="/geoip", tags=["GeoIP"])
+"""app.include_router(user_input_router, prefix="/user_input", tags=["User Input"])
+app.include_router(ai_model_router, prefix="/ai_model", tags=["AI Model"])"""
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to the playlist generator API!"}
