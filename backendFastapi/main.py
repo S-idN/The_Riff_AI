9fd0c04b1dd1fcb53f3d92b6764e366e9e64ba77
Sheron <sheronroy2003@gmail.com>
2025-02-26 14:42:13 +0530

    Added user inputs fr

--- backendFastapi/main.py
+++ backendFastapi/main.py
@@ -1,15 +1,15 @@
 # main.py
 from fastapi import FastAPI
 from services.geoip_service import geoip_router
-"""from services.user_input_service import user_input_router
-from services.ai_model_service import ai_model_router"""
+from services.userinput_service import input_router
+"""from services.ai_model_service import ai_model_router"""
 
 app = FastAPI()
 
 # Registering the routers
 app.include_router(geoip_router, prefix="/geoip", tags=["GeoIP"])
-"""app.include_router(user_input_router, prefix="/user_input", tags=["User Input"])
-app.include_router(ai_model_router, prefix="/ai_model", tags=["AI Model"])"""
+app.include_router(input_router, prefix="/user_input", tags=["User Input"])
+"""app.include_router(ai_model_router, prefix="/ai_model", tags=["AI Model"])"""
 
 @app.get("/")
 async def root():
